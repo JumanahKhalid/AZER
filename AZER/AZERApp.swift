af0a8b4f3b5884faf2e6b1257aabd82cfebdac6e
jumanah khlid albisher <110771258+JumanahKhalid@users.noreply.github.com>
2023-11-19 11:21:45 +0300

    Add files via upload

---
+++ AZER/AZERApp.swift
@@ -0,0 +1,17 @@
+//
+//  AZERApp.swift
+//  AZER
+//
+//  Created by jumanah khalid albisher on 05/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct AZERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AZER/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AZER
+//
+//  Created by jumanah khalid albisher on 05/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
